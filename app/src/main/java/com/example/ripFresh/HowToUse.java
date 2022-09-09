8af6c8412bca81ed52a58bb1eb69835b1707631a
JoshuaLacambacal <joshua.lacambacal@gmail.com>
2022-09-09 15:35:06 +0800

    HowToUse with java

---
+++ app/src/main/java/com/example/ripFresh/HowToUse.java
@@ -0,0 +1,14 @@
+package com.example.ripFresh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HowToUse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to_use);
+    }
+}
